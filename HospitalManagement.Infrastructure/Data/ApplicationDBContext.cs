d63bb4092cb2a332403297f72eada739ec6b6934
Rane <saylrane@deloitte.com>
2026-03-24 20:25:20 +0530

    Inventory and medicine update

--- HospitalManagement.Infrastructure/Data/ApplicationDBContext.cs
+++ HospitalManagement.Infrastructure/Data/ApplicationDBContext.cs
@@ -12,9 +12,12 @@ namespace HospitalManagement.Infrastructure.Data
 
         public DbSet<Patient> Patients { get; set; }
         public DbSet<Doctor> Doctors { get; set; }
-        public DbSet<Inventory> Inventorys { get; set; }
+        public DbSet<InventoryLogs> InventoryLogs { get; set; }
         public DbSet<Medicine> Medicines { get; set; }
         public DbSet<Prescription> Prescriptions { get; set; }
         public DbSet<PrescriptionItems> PrescriptionItems { get; set; }
+        public DbSet<Bill> Bill { get; set; }
+        public DbSet<Payment> Payment { get; set; }
+        public DbSet<Appointment> Appointment { get; set; }
     }
 }
